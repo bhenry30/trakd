888df1a5aedaf2b02743cdc3d190d7c906d4287e
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-03-20 13:54:17 -0500

    delete and post routes working properly and unique ids applied with uuid

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const notesJSON = require('./db/db.json')
+const uuid = require('uuid')
+let notesJSON = require('./db/db.json')
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -25,46 +26,40 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-function createNote(body, notesArr) {
+// delete note function
+function createNote(body, notesJSON) {
     const newNote = body;
+    body.id = uuid.v4();
+    notesJSON.push(newNote);
 
-    if (!Array.isArray(notesArr)) {
-    notesArr = []
-    };
-
-    body.id = notesArr[0];
-    notesArr[0]++;
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArr, null, 2)
-    );
-    return newNote;
+        JSON.stringify({notes: notesJSON}, null, 2)
+        );
+        return newNote
 }
 
-// create 
+// create note post route
 app.post('/api/notes', (req, res) => {
-     // if any data in req.body is incorrect, send 400 error back
-  if (!req.body.title) {
-    res.status(400).send('No title included');
-  } else {
-    const newNote = createNote(req.body, notesJSON)
-    notesJSON.push(newNote)
-
-  }
+    let newNote = createNote(req.body, notesJSON);
+    res.json(newNote);
 })
 
-function deleteNote(id, notesArr) {
-    
+// delete note function
+function deleteNote(id, notesJSON) {
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes: notesJSON}, null, 2)
+    );
 }
-// delete
+
+// delete route
 app.delete('/api/notes/:id', (req, res) => {
     notesJSON = notesJSON.filter(note => {
         return note.id !== req.params.id
     })
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesJSON)
-    );
+    deleteNote(req.id, notesJSON)
     return res.send();
 })
 
